71735e5cbb040e18f0fc5e02ed956d1f2c505325
Lori Shepherd <shepherl@gmail.com>
2016-10-05 17:17:39 -0400

    Initial commit
    - implement 'hi()' function

---
+++ R/hi.R
@@ -0,0 +1,21 @@
+#' Help start conversations
+#' 
+#' This function generates some helpful text that can be used to start
+#' conversations in all kinds of awkward social situtations.
+#' 
+#' @param who character(1) The name of a person you wish to start a 
+#' conversation with 
+#'  
+#' @return character(1) Line of text to be used when starting conversations  
+#' 
+#' @examples
+#' hi("Lori Shepherd")
+#' 
+#' @export
+hi <- function(who){
+    paste("Hello", shout(who), "You have", nchar(who), "letters in your name!")
+}
+
+shout <- function(who){
+    toupper(who)
+}
