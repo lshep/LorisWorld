dfc56c3cd1672be7854f9545096ff80bed4fc464
Lori Shepherd <shepherl@gmail.com>
2016-10-05 17:41:44 -0400

    Implement 'how' for hi()
    - 'shout' for all caps
    - 'whisper' for lowercase

--- R/hi.R
+++ R/hi.R
@@ -1,26 +1,36 @@
 #' Help start conversations
-#'
+
 #' This function generates some helpful text that can be used to start
 #' conversations in all kinds of awkward social situtations.
 #'
 #' @param who character(1) The name of a person you wish to start a
 #' conversation with
 #'
+#' @param how character(1) Either shout or whisper to determine intensity of
+#' output
+#'
 #' @return character(1) Line of text to be used when starting conversations
 #'
 #' @examples
 #' hi("Lori Shepherd")
+#' hi("MiniMe", "whisper")
 #'
 #' @export
-hi <- function(who){
+hi <- function(who, how=c("shout", "whisper")){
     stopifnot(
         is.character(who),
         length(who) == 1,
         !is.na(who)
     )
-    paste("Hello", shout(who), "You have", nchar(who), "letters in your name!")
+    how <- match.arg(how)
+    fun = switch(how, shout=shout, whisper=whisper)
+    paste("Hello", fun(who), "You have", nchar(who), "letters in your name!")
 }
 
 shout <- function(who){
     toupper(who)
+}
+
+whisper <- function(who){
+    tolower(who)
 }
