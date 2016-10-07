0df86b5b81416ef6c7a7fb4630dc305ef2ad7f00
Lori Shepherd <shepherl@gmail.com>
2016-10-07 16:47:59 -0400

    implement input_classic()
    - input two files and merge into a single data.frame of samples x feature

---
+++ R/classic.R
@@ -0,0 +1,24 @@
+#' Classic R approached to data analysis
+#' 
+#' The functions here illustrate how an 'old school' R usere might approach data
+#' management analysis. 
+#' 
+#' @details There are two files. They are from a microarray experiment. The 
+#' first file, ALL-phenoData.csv describes the samples. The second file, 
+#' ALL-expression.csv are the normalized experssion values for 12k probsets 
+#' across 128 samples. The files originally came form sheets in excel, and were
+#' exported from Excel using export-to-csv
+#' 
+#' @param pdata_file character(1) The path to the phenotype data file 
+#' 
+#' @param exprs_file character(1) The path to the expression data file
+#' 
+#' @return A data.frame() containing samples as rows and phenotype data and 
+#' expression data as columns.
+#' 
+#' @export
+input_classic <- function(pdata_file, exprs_file){
+    pdata <- read.csv(pdata_file, row.names=1, check.names=FALSE)
+    exprs <- read.csv(exprs_file, row.names=1, check.names=FALSE)
+    merge(pdata, t(exprs), by=0)
+}
