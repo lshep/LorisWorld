61bea24b60d4d17e60e8a32a26664ddab9f9760a
Lori Shepherd <shepherl@gmail.com>
2016-10-14 16:39:24 -0400

    Added is_scalar_character()
    - checked parameters of hi, classic, and rich with new helper function
    
    -avoiding copy and paste of identical code chunks
    
    - test inputs of functions to meet preconditions

--- R/rich.R
+++ R/rich.R
@@ -1,23 +1,26 @@
 #' Rich data input
-#' 
-#' Capture the semantic meaning of the data we represent. For instance, 
-#' separately recognizing the 'phenotypic' data (describing the sample) and the 
+#'
+#' Capture the semantic meaning of the data we represent. For instance,
+#' separately recognizing the 'phenotypic' data (describing the sample) and the
 #' 'expression' values (derived from , eg microarray or RNAseq data)
 #'
 #' @param pdata_file character(1) The path to the phenotype data file
 #'
 #' @param exprs_file character(1) The path to the expression data file
 #'
-#' @return A SummarizedExperiment() containing samples as colData(), and the 
+#' @return A SummarizedExperiment() containing samples as colData(), and the
 #'    expression values as assay()
-#'    
-#' @importFrom SummarizedExperiment SummarizedExperiment   
-#'    
+#'
+#' @importFrom SummarizedExperiment SummarizedExperiment
+#'
 #' @export
 input_rich <- function(pdata_file, exprs_file){
+    stopifnot(is_scalar_character(pdata_file))
+    stopifnot(is_scalar_character(exprs_file))
+
     pdata <- read.csv(pdata_file, row.names=1, check.names=FALSE)
     exprs <- read.csv(exprs_file, row.names=1, check.names=FALSE)
-    
-    SummarizedExperiment(as.matrix(exprs), 
+
+    SummarizedExperiment(as.matrix(exprs),
                          colData=pdata)
 }
